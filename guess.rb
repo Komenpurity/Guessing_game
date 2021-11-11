46aeea9c31976711a5840490e11198d304f62ba3
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-11-11 19:24:43 +0300

    added a for loop

--- guess.rb
+++ guess.rb
@@ -1,11 +1,14 @@
  secret_word = "Girraffe Academy"
 guess = ""
+guess_limit = 0
 
-
-while guess != secret_word
+while guess != secret_word and guess_limit < 3
     print "Enter the secret word: "
     guess = gets.chomp()
+    guess_limit +=1
 end
 
-puts "You won!"
+if guess == secret_word
+    puts "You won!"
+end
 
