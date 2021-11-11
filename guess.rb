80bb0abe195f5702fa45b5b01886ed943df46b34
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-11-11 19:19:04 +0300

    added a while loop

--- guess.rb
+++ guess.rb
@@ -1,7 +1,11 @@
  secret_word = "Girraffe Academy"
+guess = ""
 
 
- print "Enter the secret word: "
- guess = gets.chomp()
+while guess != secret_word
+    print "Enter the secret word: "
+    guess = gets.chomp()
+end
 
- 
+puts "You won!"
+
